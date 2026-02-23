44125cf34f8fa29f5fa367763a5661ca7d05af2a
Kayla Rampersaud <krampersaud98@gmail.com>
2026-02-23 00:42:56 -0500

    created a new database in PostgreSQL with a create employee function and seed to the database

--- db/seed.js
+++ db/seed.js
@@ -7,4 +7,12 @@ console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
   // TODO
+  for (let i = 0; i < 10; i++) {
+    const employee = {
+      name: "Employee " + i,
+      birthday: "1001-10-01",
+      salary: Math.floor(Math.random() * 500000),
+    };
+    await createEmployee(employee);
+  }
 }
